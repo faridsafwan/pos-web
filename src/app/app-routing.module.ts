e3da247d1c466689096153c14cef3913c4094585
faridsafwan <faridsafwan@aerodyne.co>
2021-01-07 07:56:10 +0800

    Add - components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Layouts
+import { AuthComponent } from './layouts/auth/auth.component';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+
+import { LoginComponent } from './views/auth/login/login.component';
+import { HomeComponent } from './views/dashboard/home/home.component';
+
+const routes: Routes = [
+  // Dashboard views
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ],
+  },
+
+  // Auth views
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { AuthComponent } from './layouts/auth/auth.component';
+import { HomeComponent } from './views/dashboard/home/home.component';
+import { LoginComponent } from './views/auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AuthComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
