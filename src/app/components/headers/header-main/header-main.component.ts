db3909fb0b7bf560d284874c311d3fd9419950e2
faridsafwan <faridsafwan@aerodyne.co>
2021-01-31 13:37:19 +0800

    refactor(header main): Add class condition

--- src/app/components/headers/header-main/header-main.component.ts
+++ src/app/components/headers/header-main/header-main.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header-main',
   templateUrl: './header-main.component.html',
-  styleUrls: ['./header-main.component.scss']
+  styleUrls: ['./header-main.component.scss'],
 })
 export class HeaderMainComponent implements OnInit {
+  @Input() isUse = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
